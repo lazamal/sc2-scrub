67af5909b743bca90e8619f623be65082208d7af
Dor Lazarov <lazamal@gmail.com>
2025-09-28 23:47:53 +0300

    Added table headlines using map to track.jsx

--- src/pages/Track.jsx
+++ src/pages/Track.jsx
@@ -1,10 +1,28 @@
 import Navbar from "../components/Navbar";
 
 export default function Track() {
+  const progressTable = [
+    "Build name",
+    "Target hit benchmark",
+    "Best time hit",
+    "map",
+    "W/L ratio",
+    "Cheese rating",
+    "Standard rating",
+  ];
   return (
-    <>
+    <div className="min-h-screen">
       <Navbar />
-      <h1>Track your excecution</h1>
-    </>
+
+      <div className="pt-6 flex flex-row gap-20">
+        {progressTable.map((headline, index) => {
+          return (
+            <p className="text-white font-semibold" key={index}>
+              {headline}
+            </p>
+          );
+        })}
+      </div>
+    </div>
   );
 }

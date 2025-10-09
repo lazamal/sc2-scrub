f49f5d42763edd410c32263d9f18ef38fbfb71dd
lazamal <lazamal@gmail.com>
2025-10-09 23:29:57 +0300

    consistent style

--- src/styles.js
+++ src/styles.js
@@ -4,4 +4,4 @@ export const buttonStyle = `px-5 py-2
   hover:bg-cyan-500 hover:shadow-[0_0_10px_#22d3ee] 
   active:scale-95 transition`;
 
-export const replayTabStyle = `bg-blue-800 w-200 h-150 flex flex-col justify-center items-center border-2 text-black pb-4`;
+export const replayTabStyle = `bg-cyan-800 w-200 h-150 flex flex-col justify-center items-center border-2 text-black pb-4`;

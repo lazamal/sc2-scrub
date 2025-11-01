634b6fb3882190cd5e3e2e7946965c58cab85d33
Dor Lazarov <lazamal@gmail.com>
2025-11-01 14:28:01 +0200

    practicing promises

--- src/promisePractice.js
+++ src/promisePractice.js
@@ -1,1 +1,30 @@
-const promise = new Promise((resolve, reject) => {});
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Done!");
+    }, duration);
+  });
+  return promise;
+};
+
+function getMyLocation() {
+  navigator.geolocation.getCurrentPosition(
+    (locationData) => {
+      console.log("Getting your location...");
+      setTimer(1000)
+        .then((data) => {
+          // this get the parameter from the resolve function above
+          console.log(data);
+        })
+        .then(() => {
+          // this is getting the location data recived from the geolocation function
+          console.log(locationData);
+        });
+    },
+    () => {
+      console.log("Error getting the location");
+    }
+  );
+}
+
+getMyLocation();
